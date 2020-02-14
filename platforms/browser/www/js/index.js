f99741b8031090971f89f3c6325983754da9cdb7
luisclaudio2302 <luisclaudio2302lcn01@gmail.com>
2020-02-13 22:48:35 -0300

    tela login funcional

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,46 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var apiserver = "http://172.16.21.156/voteshirt/api.php";
+var participante = null;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function irTelaPrincipal(){
+		$(".pagina").fadeOut('slow',function(){
+			$("#pagina-principal").fadeIn('slow');	
+		});
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+function eventos(){
+	$("#link-cadastro").click(function(){
+		
+		$(".pagina").fadeOut('slow',function(){
+			$("#pagina-cadastro").fadeIn('slow');	
+		});
+		
+	})
+	
+	
+	$("#btentrar").click(function(){
+		    var email = $("#login-email").val();
+			var senha = $("#login-senha").val();
+			$.ajax({
+				url: apiserver,
+				method: 'post',
+				crossDomain : true,
+				data: {  op: 'va',email: email, senha: senha }
+			}).done(function(msg){
+				console.log(msg);
+			    if (msg!=null && msg.length>2) {
+					participante = JSON.parse(msg);
+					if (participante[0].id!=null && participante[0].id > 0) {
+						
+						irTelaPrincipal();
+						
+					} else {
+						M.toast({html:"Email ou Senha inválida"});
+					}
+				} else {
+					M.toast({html:"Email ou Senha inválida"});
+				}
+				
+				
+			});
+		
+		
+	});
+	
+	
+}
 
-app.initialize();
+
+
+$(document).ready( function(){
+	  M.AutoInit(); 
+      $('.sidenav').sidenav();
+	  eventos();
+});
--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,15 @@
-function eventos(){
-	
-	$("#btentrar").click(function(){
-		
+var apiserver = "http://172.16.21.156/voteshirt/api.php";
+var participante = null;
+
+
+function irTelaPrincipal(){
 		$(".pagina").fadeOut('slow',function(){
-		   $("#pagina-principal").fadeIn('slow');	
+			$("#pagina-principal").fadeIn('slow');	
 		});
-		
-	})
-	
+}
+
+
+function eventos(){
 	$("#link-cadastro").click(function(){
 		
 		$(".pagina").fadeOut('slow',function(){
@@ -16,20 +18,43 @@ function eventos(){
 		
 	})
 	
-	$("#btfinalizar").click(function(){
+	
+	$("#btentrar").click(function(){
+		    var email = $("#login-email").val();
+			var senha = $("#login-senha").val();
+			$.ajax({
+				url: apiserver,
+				method: 'post',
+				crossDomain : true,
+				data: {  op: 'va',email: email, senha: senha }
+			}).done(function(msg){
+				console.log(msg);
+			    if (msg!=null && msg.length>2) {
+					participante = JSON.parse(msg);
+					if (participante[0].id!=null && participante[0].id > 0) {
+						
+						irTelaPrincipal();
+						
+					} else {
+						M.toast({html:"Email ou Senha inválida"});
+					}
+				} else {
+					M.toast({html:"Email ou Senha inválida"});
+				}
+				
+				
+			});
 		
-		$(".pagina").fadeOut('slow',function(){
-			$("#pagina-login").fadeIn('slow');	
-		});
 		
-	})
+	});
+	
 	
 }
 
 
 
 $(document).ready( function(){
-	   
+	  M.AutoInit(); 
       $('.sidenav').sidenav();
 	  eventos();
 });
